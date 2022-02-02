ad216b5006f971e81193589fbf7f550fb5518959
esuter19 <59849377+esuter19@users.noreply.github.com>
2022-02-01 20:48:43 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-$.getJSON("restaurants.geojson",function(data){
+$.getJSON("restaurant.geojson",function(data){
     L.geoJson(data).addTo(map);
   });
